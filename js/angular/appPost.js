45e61dfa4dd0e694cd14b7a6763f9a7756661396
unknown <luismondragon1121>
2019-02-06 14:00:16 -0600

    prueba de archivos

--- js/angular/appPost.js
+++ js/angular/appPost.js
@@ -3,7 +3,7 @@ var myApp = angular.module('app', ['ngSanitize', 'ngMask']);
 
 myApp.controller('entradas', ['$scope','$window','$http', function($scope,$window,$http) {
 
-  $http.get('http://localhost/sitio_coine/blog/?rest_route=/wp/v2/posts&_embed', {
+  $http.get('http://localhost/new_coine/blog/?rest_route=/wp/v2/posts&_embed', {
     headers: {'Authorization': 'Basic '+btoa('coine_blog:coine_MKT2019') }
    
   }).then(function (res){

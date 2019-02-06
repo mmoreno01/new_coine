04fa0ce6e40e231d402676947725c96a33924825
MIGUEL ANGEL MORENO <you@example.com>
2019-02-06 10:37:00 -0600

    creacion de blog noticas

--- js/angular/appPost.js
+++ js/angular/appPost.js
@@ -3,8 +3,8 @@ var myApp = angular.module('app', ['ngSanitize', 'ngMask']);
 
 myApp.controller('entradas', ['$scope','$window','$http', function($scope,$window,$http) {
 
-  $http.get('http://www.bilanz.mx/blog/?rest_route=/wp/v2/posts&_embed', {
-    headers: {'Authorization': 'Basic '+btoa('user_blog:user_MKT_2018') }
+  $http.get('http://localhost/sitio_coine/blog/?rest_route=/wp/v2/posts&_embed', {
+    headers: {'Authorization': 'Basic '+btoa('coine_blog:coine_MKT2019') }
    
   }).then(function (res){
     $scope.postConsulta = res.data;

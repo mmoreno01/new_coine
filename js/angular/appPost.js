70ac96dc0db1e053580cc16c6ed39e44560346a4
MIGUEL ANGEL MORENO <you@example.com>
2019-02-06 10:42:43 -0600

    creacion de dev notic

--- js/angular/appPost.js
+++ js/angular/appPost.js
@@ -4,7 +4,7 @@ var myApp = angular.module('app', ['ngSanitize', 'ngMask']);
 myApp.controller('entradas', ['$scope','$window','$http', function($scope,$window,$http) {
 
   $http.get('http://localhost/sitio_coine/blog/?rest_route=/wp/v2/posts&_embed', {
-    headers: {'Authorization': 'Basic '+btoa('coine_blog:coine_MKT2019') }
+    headers: {'Authorization': 'Basic '+btoa('') }
    
   }).then(function (res){
     $scope.postConsulta = res.data;

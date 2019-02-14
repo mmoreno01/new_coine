cf559a62f66ac094047ae493b916cc50d103a4eb
MIGUEL ANGEL MORENO <you@example.com>
2019-02-14 13:09:57 -0600

    se cambiaron baners generales

---
+++ js/slick-conocenos.js
@@ -0,0 +1,18 @@
+$('.filtering').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4
+});
+
+var filtered = false;
+
+$('.js-filter').on('click', function(){
+  if (filtered === false) {
+    $('.filtering').slick('slickFilter',':even');
+    $(this).text('Unfilter Slides');
+    filtered = true;
+  } else {
+    $('.filtering').slick('slickUnfilter');
+    $(this).text('Filter Slides');
+    filtered = false;
+  }
+});

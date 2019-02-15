88274ea493d652bd8989f9358288c980a1503cf3
MIGUEL ANGEL MORENO <you@example.com>
2019-02-15 17:10:37 -0600

    creacion de seccion noticias en home

--- js/angular/appPost.js
+++ js/angular/appPost.js
@@ -12,3 +12,16 @@ myApp.controller('entradas', ['$scope','$window','$http', function($scope,$windo
   });
 }]);
 
+
+
+myApp.controller('entradasNotices', ['$scope','$window','$http', function($scope,$window,$http) {
+
+  $http.get('http://www.coine.lat/coine_noticias/?rest_route=/wp/v2/posts&_embed', {
+    headers: {'Authorization': 'Basic '+btoa('user_noticias:DNHV$Dr0(*jJEg0uMV') }
+   
+  }).then(function (res){
+    $scope.postNotices = res.data;
+    console.log($scope.postNotices);
+  });
+}]);
+

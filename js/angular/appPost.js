55fac74974b705fc31526e413eeef00d878da89c
MIGUEL ANGEL MORENO <you@example.com>
2019-04-02 09:22:53 -0600

    se realizaron correociones en general

--- js/angular/appPost.js
+++ js/angular/appPost.js
@@ -8,17 +8,18 @@ myApp.controller('entradas', ['$scope','$window','$http', function($scope,$windo
    
   }).then(function (res){
     $scope.postConsulta = res.data;
-    console.log($scope.postConsulta);
   });   
 }]);  
  
 myApp.controller('entradasNotices', ['$scope','$window','$http', function($scope,$window,$http) {
 
-  $http.get('http://www.coine.lat/coine_noticias/?rest_route=/wp/v2/posts&_embed', {
+  $http.get('http://www.coine.lat/coine_noticias/?rest_route=/wp/v2/posts&_embed&categories=4,5', {
     headers: {'Authorization': 'Basic '+btoa('user_noticias:DNHV$Dr0(*jJEg0uMV') }
    
   }).then(function (res){
     $scope.postNotices = res.data;
+   
+
     console.log($scope.postNotices);
   });
 }]);
